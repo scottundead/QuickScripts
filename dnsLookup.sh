138bb719ad410ef64d5073ad91dd47737f82cb3d
ScottUndead <scottsadler@outlook.com>
2017-02-06 15:23:17 -0500

    Implemented cymru's bulk lookup into whois lookup script

--- dnsLookup.sh
+++ dnsLookup.sh
@@ -2,5 +2,5 @@
 
 for ip_address in `cat input.txt`;
 do
-      dig +short -x $ip_address >> dnsLookup.out
+    dig +short -x $ip_address > dnsLookup.out
 done
--- whoisLookup.sh
+++ whoisLookup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-for ip_address in `cat input.txt`;
-do
+if [[ $(wc -l <input.txt) -ge 2 ]]
+then
+    `echo "begin" > bulkInput.txt`
+    `echo "verbose" >> bulkInput.txt`
+    `cat input.txt >> bulkInput.txt`
+    `echo -e "\nend" >> bulkInput.txt`
+    `nc whois.cymru.com 43 < bulkInput.txt | sort -n > whoisLookup.out`
+    `rm bulkInput.txt`
+else
+    for ip_address in `cat input.txt`;
+	do
         whois -h whois.cymru.com " -v $ip_address" >> whoisLookup.out
-done
+		done
+fi
